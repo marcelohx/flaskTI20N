6a6349380ef2c755f424172b9dcf5576a7627d7b
MARCELO HENRIQUE XAVIER <marcelo.hxavier@senacsp.edu.br>
2024-10-09 22:09:21 -0300

    Terceiro Commit

--- app/routes.py
+++ app/routes.py
@@ -8,14 +8,18 @@ link = "https://flaskti20nmarcelo-default-rtdb.firebaseio.com/" #conectar ao ban
 @app.route('/index')
 def index():
     return render_template('index.html', titulo="Página Principal")
-@app.route('/contato')
-def contato():
-    return render_template('contato.html', titulo="Contato")
-
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html', titulo="Cadastro")
-
+@app.route('/consultas')
+def consultar():
+    return render_template('consultas.html', titulo="Consultar")
+@app.route('/atualizar')
+def atualizacao():
+    return render_template('atualizar.html', titulo="Atualizar")
+@app.route('/excluir')
+def exclusao():
+    return render_template('excluir.html', titulo="Excluir")
 @app.route('/cadastrarUsuario',methods=['POST'])
 def cadastrarUsuario():
     try:
@@ -28,7 +32,6 @@ def cadastrarUsuario():
         return 'Cadastrado com sucesso!'
     except Exception as e:
         return f'Ocorreu um erro\n\n {e}'
-
 @app.route('/listar')
 def listarTudo():
     try:
@@ -37,7 +40,6 @@ def listarTudo():
         return dicionario
     except Exception as e:
         return f'Algo deu errado \n\n{e}'
-
 @app.route('/listarIndividual', methods=['POST'])
 def listarIndividual():
     try:
@@ -45,7 +47,6 @@ def listarIndividual():
         dicionario = requisicao.json()
         cpf = request.form.get("cpf")
         idCadastro = "" #Armazenar o ID indiviual
-
         for codigo in dicionario:
             chave = dicionario[codigo]['cpf']
             if chave == cpf:
@@ -56,20 +57,38 @@ def listarIndividual():
                 return f'id do Cadastro: {idCadastro}\nNome: {nome}\nEndereco: {endereco}\nTelefone: {telefone}'
     except Exception as e:
         return f'Algo deu errado \n\n{e}'
-
-@app.route('/atualizar')
+@app.route('/atualizar', methods=['POST'])
 def atualizar():
     try:
-        dados = {"nome":"João"}
-        requisicao = requests.patch(f'{link}/cadastrar/-O89htddyr8QVQxaj2iI/.json', data=json.dumps(dados))
-        return "Atualizado com sucesso!"
+        if request.form.get('cpf') != "" and requests.form.get('nome') and requests.form.get('telefone') and requests.form.get('endereco'):
+            requisicao = requests.get(f'{link}/cadastrar/.json')
+            dicionario = requisicao.json()
+            cpf = request.form.get("cpf")
+            idCadastro = 0
+            for codigo in dicionario:
+                chave = dicionario[codigo]['cpf']
+                if chave == cpf:
+                    idCadastro = codigo
+                    nome = request.form.get("nome")
+                    telefone = request.form.get("telefone")
+                    endereco = request.form.get("endereco")
+                    dados = {"nome":nome,"telefone":telefone,"endereco":endereco}
+                    requisicao = requests.patch(f'{link}/cadastrar/{idCadastro}/.json', data=json.dumps(dados))
+                    return "Atualizado com Sucesso!"
     except Exception as e:
         return f'Algo deu errado\n\n {e}'
-
-@app.route('/excluir')
+@app.route('/excluir', methods=['POST'])
 def excluir():
     try:
-        requisicao = requests.delete(f'{link}/cadastrar/-O8JxobEHMlnpuI8vtY9/.json')
-        return "Excluir com sucesso!"
+        requisicao = requests.get(f'{link}/cadastrar/.json')
+        dicionario = requisicao.json()
+        cpf = request.form.get("cpf")
+        idCadastro = 0
+        for codigo in dicionario:
+            chave = dicionario[codigo]['cpf']
+            if chave == cpf:
+                idCadastro = codigo
+                requisicao = requests.delete(f'{link}/cadastrar/{idCadastro}/.json')
+                return "Excluir com sucesso!"
     except Exception as e:
-        return f"Algo deu errado\n\n {e}"
+        return f'Algo deu errado\n\n {e}'
